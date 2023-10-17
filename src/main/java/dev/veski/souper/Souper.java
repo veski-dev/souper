470e4b6ff2fb97b82b38d122e778567513fee3ff
veski <urijaquet@gmail.com>
2023-10-17 14:23:09 +0200

    v1

--- src/main/java/dev/veski/souper/Souper.java
+++ src/main/java/dev/veski/souper/Souper.java
@@ -1,5 +1,9 @@
 package dev.veski.souper;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Souper extends JavaPlugin {
@@ -7,11 +11,34 @@ public final class Souper extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getLogger().info("/soup :)");
     }
 
     @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("soup")) {
+            double randNumber = Math.random();
+            double d = randNumber * 100;
+            int randomInt = (int)d;
+
+            if (!(sender instanceof Player)) {
+                Bukkit.broadcastMessage("<Server> /soup");
+                if (randomInt == 1) {
+                    Bukkit.broadcastMessage("<EPFL_ch> ˢᵒᵘᵖ");
+                }
+                return true;
+            }
+
+            Bukkit.broadcastMessage("<" + ((Player) sender).getDisplayName() + "> /soup");
+            if (randomInt == 1) {
+                Bukkit.broadcastMessage("<EPFL_ch> ˢᵒᵘᵖ");
+            }
+            return true;
+        }
+        return true;
+    }
+    @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info("/soup :(");
     }
 }
